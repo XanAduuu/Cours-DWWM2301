8fb9eba416751622752f90583fe1ae58afaceaea
NolwennWM <47659634+NolwennWM@users.noreply.github.com>
2023-05-03 09:00:22 +0200

    correction exercice intersection

--- 04-javascript/05-intersection-observer/exercice/script.js
+++ 04-javascript/05-intersection-observer/exercice/script.js
@@ -13,3 +13,50 @@
  * 3. Ajouter l'animation de l'exercice 1 aux nouveaux paragraphes.
  * 4. Ajouter la détection du dernier paragraphe au nouveau dernier paragraphe.
  */
+
+var paragraphe = document.getElementsByTagName("p");
+var observer2 = new IntersectionObserver(function(entries){
+    // console.log("coucou");
+    for(var i = 0; i<entries.length;i++)
+    {
+        if(entries[i].isIntersecting)
+        {
+            entries[i].target.classList.add("visible");
+            observer2.unobserve(entries[i].target);
+        }
+        entries[i].target.addEventListener("transitionend", (e)=>{
+            e.target.style.transform = "translateX(0)";
+        })
+    }
+}, {threshold: 0.5});
+
+for(var i = 0; i <paragraphe.length; i++)
+{
+    observer2.observe(paragraphe[i]);
+}
+
+// exo 2 :
+
+const lastP = document.querySelector("main p:last-child");
+const obsLast = new IntersectionObserver(addMore, {rootMargin: "200px"});
+const m = document.querySelector("main");
+
+obsLast.observe(lastP);
+
+function addMore(entries)
+{
+    let ent = entries[0];
+    if(ent.isIntersecting)
+    {
+        let p;
+        for(let i = 0; i<10; i++)
+        {
+            p = document.createElement("p");
+            p.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse impedit neque non sit repudiandae unde consequatur nihil magni. Laudantium eligendi sit ipsa fugiat maxime! Libero adipisci dolor porro quod molestias!"
+            m.appendChild(p);
+            observer2.observe(p);
+        }
+        obsLast.unobserve(lastP);
+        obsLast.observe(p);
+    }
+}
