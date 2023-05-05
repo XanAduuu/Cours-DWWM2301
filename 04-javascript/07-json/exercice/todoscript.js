a2e8d98a4871873419a3babc80f43e46dac389ac
NolwennWM <47659634+NolwennWM@users.noreply.github.com>
2023-05-05 09:26:59 +0200

    correction json, début fetch

--- 04-javascript/07-json/exercice/todoscript.js
+++ 04-javascript/07-json/exercice/todoscript.js
@@ -12,3 +12,51 @@
     6. éditer la liste lorsque l'on coche ou supprime un élément.
     Bonus : Utiliser le drag and drop pour déplacer nos éléments dans la liste. il faudra penser à sauvegarder les éléments déplacé.
  */
+var newTaskInput = document.getElementById("new-task");
+var addTaskButton = document.getElementById("add-task");
+var taskList = document.getElementById("task-list");
+let todos = [];
+
+addTaskButton.addEventListener("click", function(){
+    createLi(newTaskInput.value);
+    todos.push(newTaskInput.value);
+    addToLocalStorage(todos);
+    newTaskInput.value = "";
+});
+
+function createLi(value)
+{
+    var newTask = document.createElement("li");
+    newTask.innerText = value;
+    newTask.classList.add("task");
+    newTask.addEventListener("click", function(){
+        newTask.classList.toggle("completed");
+    });
+    var deleteButton = document.createElement("button");
+    deleteButton.innerText = "X";
+    deleteButton.addEventListener("click", function(){
+        taskList.removeChild(newTask);
+        // Défaut de cette solution, supprimer le premier élément trouvé si deux ont le même nom.
+        todos.splice(todos.indexOf(value), 1);
+        addToLocalStorage(todos);
+    })
+    newTask.appendChild(deleteButton);
+    taskList.appendChild(newTask);
+}
+
+function addToLocalStorage(todos)
+{
+    localStorage.setItem("todo", JSON.stringify(todos));
+}
+
+function getFromLocalStorage()
+{
+    const todoStr = localStorage.getItem("todo");
+    if(todoStr)
+    {
+        todos = JSON.parse(todoStr);
+        todos.forEach(createLi);
+    }
+        
+}
+getFromLocalStorage();
