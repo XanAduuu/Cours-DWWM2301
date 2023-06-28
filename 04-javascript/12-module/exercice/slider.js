f6206eedac0d5d27d6c2fcbf84b211cf69e34727
Xan <amexstramxgram@gmail.com>
2023-06-28 17:04:55 +0200

    exo slider

---
+++ 04-javascript/12-module/exercice/slider.js
@@ -0,0 +1,4 @@
+const img =document.querySelectorAll("img");
+img[0].src = "../../../ressources/images/paysage/lava.jpg";
+img[1].scr = "../../../ressources/images/paysage/montagne.jpg";
+img[2].src = "../../../ressources/images/paysage/space.jpg";
