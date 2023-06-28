b40c5668ab4b2f42c35658cc3fb1624f90778395
NolwennWM <47659634+NolwennWM@users.noreply.github.com>
2023-06-28 08:52:14 +0200

    correction animate

--- 04-javascript/11-animate/exercice/script.js
+++ 04-javascript/11-animate/exercice/script.js
@@ -5,3 +5,24 @@
     Puis disparaître, changer le texte, faire réaparaître la barre 
     Puis refaire le slide depuis les barres avec le nouveau texte.
 */
+const spans = document.querySelectorAll('span');
+
+function blink()
+{
+    for(let i = 0; i < spans.length; i++)
+    {
+        anime2(spans[i]);
+    }
+}
+
+async function anime2(sp)
+{
+    let indent = sp.animate({textIndent: [0]}, {duration: 2000, fill: "forwards"});
+    await indent.finished;
+    let blink = sp.animate({opacity: [0]}, {duration: 2000, fill: "forwards"});
+    await blink.finished;
+    sp.textContent = sp.dataset.text;
+    blink.reverse();
+    indent.play();
+}
+blink();
