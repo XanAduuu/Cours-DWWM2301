92796822599c96b80c9eb768df7ed1c17cfefc7e
XanAduuu <130441918+XanAduuu@users.noreply.github.com>
2023-06-29 16:27:36 +0200

    juste prix js

---
+++ 04-javascript/13-poo/01-object/exercice/script.js
@@ -0,0 +1,15 @@
+"use strict"
+import J from "./JustePrix.js";
+
+J.myJustePrix;
+console.log(J.myJustePrix);
+document.querySelector(".appli").append(J.myJustePrix());
+
+J.getNumber;
+console.log(J.getNumber);
+
+const input = document.querySelector("input");
+input.addEventListener(input, J.startJ);
+
+
+
