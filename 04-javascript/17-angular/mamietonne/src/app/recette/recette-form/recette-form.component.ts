c257f5d199b86937c52409b7c65472f2fe62a31c
NolwennWM <47659634+NolwennWM@users.noreply.github.com>
2023-07-20 09:10:36 +0200

    exercice form

--- 04-javascript/17-angular/mamietonne/src/app/recette/recette-form/recette-form.component.ts
+++ 04-javascript/17-angular/mamietonne/src/app/recette/recette-form/recette-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recette } from '../Recette';
 
 @Component({
   selector: 'app-recette-form',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./recette-form.component.scss']
 })
 export class RecetteFormComponent {
-
+  @Input() recette?: Recette;
 }
