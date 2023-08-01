80dc4b994626fbdb1cd5a7006eee3bc7f79b8a3a
NolwennWM <47659634+NolwennWM@users.noreply.github.com>
2023-08-01 10:30:34 +0200

    debut doctrine

--- 06-php/06-symfony/superProjet/src/Controller/HomeController.php
+++ 06-php/06-symfony/superProjet/src/Controller/HomeController.php
@@ -29,14 +29,30 @@ class HomeController extends AbstractController
         requirements:["username"=>"^[a-zA-Z]+$"])]
     public function hello($username): RedirectResponse
     {
-        dd("Hello ".$username);
-        return $this->redirectToRoute("bonjour");
+        // dd("Hello ".$username);
+
+        $this->addFlash("redirect", "Vous avez été redirigé");
+        $this->addFlash("Hello", "Hello $username");
+
+        return $this->redirectToRoute("bonjour", [
+            "nom"=>"inconnu",
+            "prenom"=> $username
+        ]);
     }
     #[Route("/bonjour/{nom<^[a-zA-Z]+$>}/{prenom<^[a-zA-Z]+$>?Jean}", name: "bonjour")]
     public function bonjour($nom, $prenom, Request $request): Response
     {
         dump($request);
         // dd($request);
+
+        $sess = $request->getSession();
+        if ($sess->has("nbVisite")) $nb = $sess->get("nbVisite")+1;
+        else  $nb=1 ;
+
+        $sess->set("nbVisite", $nb);
+
+        $this->addFlash("Bonjour", "Bonjour $prenom $nom");
+
         return $this->render("home/bonjour.html.twig",[
             "nom"=> $nom,
             "prenom"=>$prenom
